403c14af22e017950478adb862d5cb344dbae375
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2022-12-16 08:46:43 -0300

    basic cleanup of files and styling for wf

---
+++ learning-dutch-is-easy/src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './WF.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header></header>
+        <main></main>
+      </div>
+    );
+  }
+}
+
+export default App;
